a186795981f9e94999cbd606db464e22da2a3af9
IlyasFounas <founas.ilyas@gmail.com>
2025-07-06 18:02:38 +0200

    ✨ ex01 started

---
+++ ex01/class/PhoneBook.cpp
@@ -0,0 +1,9 @@
+#include "../includes/PhoneBook.hpp"
+
+PhoneBook::PhoneBook() {
+    std::cout << "PhoneBook open" << std::endl;
+}
+
+PhoneBook::~PhoneBook() {
+    std::cout << "PhoneBook closed" << std::endl;
+}
---
+++ ex01/main.cpp
@@ -0,0 +1,7 @@
+#include "includes/PhoneBook.hpp"
+
+int main()
+{
+    PhoneBook book1;
+    return (0);
+}
